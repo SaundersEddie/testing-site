8fa3166929328a1c0fc4aa02dcc2d091d10a167b
Eddie Saunders <saunders.eddie@outlook.com>
2021-02-04 11:36:34 -0500

    Added in button click event handler

--- assets/script.js
+++ assets/script.js
@@ -3,3 +3,8 @@
 // edwyn.saunders@outlook.com
 
 console.log ("Our Script has run!")
+
+document.getElementById("id-myBtn").addEventListener("click", function() {
+    console.log ("MyBtn has been clicked!!!");
+    document.getElementById("myText").innerHTML="My button has been clicked!!";
+});
